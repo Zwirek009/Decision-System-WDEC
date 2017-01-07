ce17c42e62d1a2ac84a6e01f31f01766c5697e6f
Zwirek009 <maciek.wiraszka@gmail.com>
2017-01-07 23:16:06 +0100

    add live data check

--- data_visualization.py
+++ data_visualization.py
@@ -2,61 +2,72 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# import data from previous simulation
-data = {}						# data gained from one result file
-combined_data = []				# list of sigle result file data	
-sim_table = {}					# combined data
-base_result_name = 'wyniki{}.xls'	# .xls name scheme
+def load_data(result_name):
+	data = {}						# data gained from one result file
+	combined_data = []				# list of sigle result file data	
+	sim_table = {}					# combined data
 
-result_name = base_result_name.format(1)			# name of first .xls file
-data = pd.read_excel(result_name, header=None)	# load the file
-combined_data.append(data)						# insert the data to list
+	temp_result_name = result_name.format(1)			# name of first .xls file
+	data = pd.read_excel(temp_result_name, header=None)	# load the file
+	combined_data.append(data)							# insert the data to list
 
-for num in range(2, 6):								# uploading other .xls files
-	result_name = base_result_name.format(num)
-	try:
-		data = pd.read_excel(result_name, header=None)
-		data = data.ix[:, 2]							# get only third column of uploaded data file
-		combined_data.append(data)
-	except FileNotFoundError:
-		print ("File " + result_name + " not found.")
+	for num in range(2, 6):								# uploading other .xls files
+		temp_result_name = result_name.format(num)
+		try:
+			data = pd.read_excel(temp_result_name, header=None)
+			data = data.ix[:, 2]							# get only third column of uploaded data file
+			combined_data.append(data)
+		except FileNotFoundError:
+			print ("File " + temp_result_name + " not found.")
 
-# concat all uploaded data to one data set
-sim_table = pd.concat(combined_data, axis=1)
-print(sim_table)
+	# concat all uploaded data to one data set
+	sim_table = pd.concat(combined_data, axis=1)
+	print(sim_table)
 
-# create separate data sets for different data
-runda = sim_table.ix[0, 1:]
-sprzedaz = sim_table.ix[1, 1:]
-udzial = sim_table.ix[2, 1:]
-wynik_firmy = sim_table.ix[3, 1:]
-gotowka = sim_table.ix[4, 1:]
-wolumen = sim_table.ix[5, 1:]
-jakosc = sim_table.ix[6, 1:]
-cena = sim_table.ix[7, 1:]
-reklama_tv = sim_table.ix[8, 1:]
-reklama_int = sim_table.ix[9, 1:]
-reklama_mag = sim_table.ix[10, 1:]
+	sub_data = {}
+	# create separate data sets for different data
+	sub_data["runda"] = sim_table.ix[0, 1:]
+	sub_data["sprzedaz"] = sim_table.ix[1, 1:]
+	sub_data["udzial"] = sim_table.ix[2, 1:]
+	sub_data["wynik_firmy"] = sim_table.ix[3, 1:]
+	sub_data["gotowka"]= sim_table.ix[4, 1:]
+	sub_data["wolumen"] = sim_table.ix[5, 1:]
+	sub_data["jakosc"] = sim_table.ix[6, 1:]
+	sub_data["cena"] = sim_table.ix[7, 1:]
+	sub_data["reklama_tv"] = sim_table.ix[8, 1:]
+	sub_data["reklama_int"] = sim_table.ix[9, 1:]
+	sub_data["reklama_mag"] = sim_table.ix[10, 1:]
 
-print(sprzedaz)
+	return sub_data
 
-# example plot
-#plt.figure('Sztuki towaru')
-#num_bins = 5
-# the histogram of the data
-#n, bins, patches = plt.hist(sprzedaz, num_bins, facecolor='green')
-#plt.plot(runda, wolumen, 'r-')
-#plt.legend(['Sprzedane', 'Wyprodukowane'])
-#plt.xlabel('Runda')
-#plt.ylabel('Liczba sztuk')
-#plt.title('Sztuki towaru')
+def main():
+	new_result_name = 'wyniki{}.xls'			# .xls name scheme
+	old_result_name = 'old_games/wyniki{}.xls'
 
-# example plot
-plt.figure('Sztuki towaru')
-plt.plot(runda, sprzedaz, 'b-')
-plt.plot(runda, wolumen, 'r-')
-plt.legend(['Sprzedane', 'Wyprodukowane'])
-plt.xlabel('Runda')
-plt.ylabel('Sztuki towaru')
+	# import data from previous simulation
+	old_data = load_data(old_result_name)
+	new_data = load_data(new_result_name)
 
-plt.show()
+	# example plot
+	#plt.figure('Sztuki towaru')
+	#num_bins = 5
+	# the histogram of the data
+	#n, bins, patches = plt.hist(sprzedaz, num_bins, facecolor='green')
+	#plt.plot(runda, wolumen, 'r-')
+	#plt.legend(['Sprzedane', 'Wyprodukowane'])
+	#plt.xlabel('Runda')
+	#plt.ylabel('Liczba sztuk')
+	#plt.title('Sztuki towaru')
+
+	# example plot
+	plt.figure('Sztuki towaru')
+	plt.plot(old_data["runda"], old_data["sprzedaz"], 'b-')
+	plt.plot(new_data["runda"], new_data["sprzedaz"], 'r-')
+	plt.legend(['Sprzedane old', 'Sprzedane new'])
+	plt.xlabel('Runda')
+	plt.ylabel('Sztuki towaru')
+
+	plt.show()
+
+if __name__ == "__main__":
+    main()
